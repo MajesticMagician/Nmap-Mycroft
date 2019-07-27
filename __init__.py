cdefbcb59e9aea6b5ad831f1ba8878abe8102769
MajesticMagician <franksaaron57@gmail.com>
2019-07-27 12:38:23 -0600

    production-test

--- __init__.py
+++ __init__.py
@@ -55,9 +55,21 @@ class TemplateSkill(MycroftSkill):
         ipAddress = message.data.get("IPADDRESS", None)
         ipaddr = str(ipAddress)
         ipaddr.replace(" ", "", 4)
-        nmScan = nmap.PortScanner()
-        nmScan.scan(ipaddr, '21-443')
-        self.speak_dialog("count.is.now", data={"count": self.count})
+        nmScan.scan('127.0.0.1', '21-443')
+        for host in nmScan.all_hosts():
+            print('Host : %s (%s)' % (host, nmScan[host].hostname()))
+            print('State : %s' % nmScan[host].state())
+            for proto in nmScan[host].all_protocols():
+                print('----------')
+                print('Protocol : %s' % proto)
+
+                lport = nmScan[host][proto].keys()
+                lport.sort()
+                for port in lport:
+                    print ('port : %s\tstate : %s' % (port, nmScan[host][proto][port]['state'])
+                    self.speak_dialog("nmap.scan", data={"results": 'port : %s\tstate : %s' % (port, nmScan[host][proto][port]['state']})
+
+        self.speak_dialog("nmap.scan", data={"results": })
 
     # The "stop" method defines what Mycroft does when told to stop during
     # the skill's execution. In this case, since the skill's functionality
