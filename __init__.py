598faaa48615130e1230bc4d5451a3054b990b8e
MajesticMagician <franksaaron57@gmail.com>
2019-07-27 12:53:22 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -56,17 +56,13 @@ class TemplateSkill(MycroftSkill):
         ipaddr = str(ipAddress)
         ipaddr.replace(" ", "", 4)
         nmScan.scan('127.0.0.1', '21-443')
-        for host in nmScan.all_hosts():
-            print('Host : %s (%s)' % (host, nmScan[host].hostname()))
-            print('State : %s' % nmScan[host].state())
-            for proto in nmScan[host].all_protocols():
-                print('----------')
-                print('Protocol : %s' % proto)
-
-                lport = nmScan[host][proto].keys()
-                lport.sort()
-                for port in lport:
-                    print ('port : %s\tstate : %s' % (port, nmScan[host][proto][port]['state'])
+        print('Host : %s (%s)' % (host, nmScan[host].hostname()))
+        print('State : %s' % nmScan[host].state())
+        print('----------')
+        print('Protocol : %s' % proto)
+        lport = nmScan[host][proto].keys()
+        lport.sort
+        print ('port : %s\tstate : %s' % (port, nmScan[host][proto][port]['state'])
 
     # The "stop" method defines what Mycroft does when told to stop during
     # the skill's execution. In this case, since the skill's functionality
